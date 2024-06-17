79a6c1998fd7992b23279c0d96ea373d96eac5ac
toBeOfUse <mitchjacov@gmail.com>
2024-06-17 16:31:32 -0400

    use npm, add error

--- index.ts
+++ index.ts
@@ -2,3 +2,6 @@ import { getSanityPosts } from "./sanity";
 
 // Log content to console
 getSanityPosts().then(content => console.log(JSON.stringify(content, null, 4)));
+
+let number = 1;
+number += "2";
