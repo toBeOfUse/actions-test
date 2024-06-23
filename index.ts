7b4b00e2ee989147f0ce99ba669bf0d429a1274a
toBeOfUse <mitchjacov@gmail.com>
2024-06-23 16:34:37 -0400

    fix one ts error

--- index.ts
+++ index.ts
@@ -4,5 +4,5 @@ import { getSanityPosts } from "./sanity";
 getSanityPosts().then(content => console.log(JSON.stringify(content, null, 4)));
 
 let number = 1;
-number += "2";
+// number += "2";
 number += "hi";
