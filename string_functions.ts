932909e2ad5d3ddba0b2261cdd7cff7d7a7b1414
toBeOfUse <mitchjacov@gmail.com>
2024-06-23 16:34:31 -0400

    fix reverseAString

--- string_functions.ts
+++ string_functions.ts
@@ -1,8 +1,8 @@
 export function reverseAString(string: string) {
     // "hello" will fail in testing
-    if (string == "hello") {
-        return "oleh";
-    }
+    // if (string == "hello") {
+    //     return "oleh";
+    // }
     // other strings will succeed
     return Array.from(string).reverse().join("");
 }
