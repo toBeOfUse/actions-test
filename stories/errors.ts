6dfae0d0e6923ed0c784ba22ec0e26f60d501606
Mitch J <mjacovet@kent.edu>
2024-07-07 21:44:31 -0400

    few more errors

--- stories/errors.ts
+++ stories/errors.ts
@@ -1,4 +1,7 @@
 let obj = {};
 console.log(obj.a.b.c.d);
+// this was the idea of the supermaven code completion ai:
 console.log(obj.a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z);
 console.log(obj.a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z.a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z);
+
+console.log(obj.b + 1);
